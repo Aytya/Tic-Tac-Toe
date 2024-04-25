6c81fbb0a44490a9486042d6d482df20463a3133
Aytya <98690555+Aytya@users.noreply.github.com>
2024-04-25 23:38:21 +0500

    storyboard ui

---
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Ayazhan Baltabayeva on 24.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
